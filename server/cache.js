ed91a441d46645e9a27cb169c825c072a1c484c6
Gabrielle Rivera <gcrivera@mit.edu>
2019-04-21 14:38:56 -0400

    fixing redis connection for heroku

--- server/cache.js
+++ server/cache.js
@@ -3,7 +3,7 @@
  */
 
 const Redis = require('ioredis')
-const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST)
+const redis = new Redis(process.env.REDIS_URL)
 
 module.exports = {
   /** Koa middleware function to check cache before continuing to any endpoint handlers */
