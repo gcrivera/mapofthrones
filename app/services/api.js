1a725bd45cb85d942ee07471c7e2dfedceeac839
Mary Thielking <mthielki@mit.edu>
2019-04-21 09:23:50 -0400

    Remove unused services code

--- app/services/api.js
+++ app/services/api.js
@@ -17,36 +17,4 @@ export class ApiService {
   async getEpisode(seasonNum, episodeNum) {
     return this.httpGet(`episodes/${seasonNum}/${episodeNum}`);
   }
-
-  // async getLocations (type) {
-  //   return this.httpGet(`locations/${type}`)
-  // }
-
-  // async getLocationSummary (id) {
-  //   return this.httpGet(`locations/${id}/summary`)
-  // }
-
-  // async getKingdoms () {
-  //   return this.httpGet('kingdoms')
-  // }
-
-  // async getKingdomSize (id) {
-  //   return this.httpGet(`kingdoms/${id}/size`)
-  // }
-
-  // async getCastleCount (id) {
-  //   return this.httpGet(`kingdoms/${id}/castles`)
-  // }
-
-  // async getKingdomSummary (id) {
-  //   return this.httpGet(`kingdoms/${id}/summary`)
-  // }
-
-  // async getAllKingdomDetails (id) {
-  //   return {
-  //     kingdomSize: await this.getKingdomSize(id),
-  //     castleCount: await this.getCastleCount(id),
-  //     kingdomSummary: await this.getKingdomSummary(id)
-  //   }
-  // }
 }
